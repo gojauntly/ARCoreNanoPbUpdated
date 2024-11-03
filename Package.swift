38dee7332085bfb5dd4941de5835eed56a12ba74
hlefe <hugo.lefevre@gmail.com>
2024-11-03 14:19:44 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -27,9 +27,9 @@ let package = Package(
     .library(name: "ARCoreSemantics", targets: ["Semantics"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/firebase/nanopb.git", "2.30910.0" ..< "2.30911.0"),
+    .package(url: "https://github.com/firebase/nanopb.git", "2.30910" ..< "2.30911.0"),
     .package(
-      url: "https://github.com/google/GoogleDataTransport.git", .upToNextMajor(from: "9.2.0")),
+      url: "https://github.com/google/GoogleDataTransport.git", .upToNextMajor(from: "10.1.0")),
     .package(url: "https://github.com/google/gtm-session-fetcher.git", "2.1.0"..<"4.0.0"),
     .package(url: "https://github.com/firebase/firebase-ios-sdk.git", "8.0.0"..<"11.0.0"),
   ],
